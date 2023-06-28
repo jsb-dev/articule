3cbdc2761fc611f10fb168f84251c5285bb552ef
jsb-dev <jsb-dev@outlook.com>
2023-06-28 14:03:59 +1000

    Introduce new MUI-based template for TopicNode

--- client/src/components/diagram/diagram-components/nodes/TopicNode.jsx
+++ client/src/components/diagram/diagram-components/nodes/TopicNode.jsx
@@ -1,8 +1,22 @@
 import React, { useState } from 'react';
 import { Handle, Position } from 'reactflow';
-import Modal from 'react-modal';
+import {
+  Modal,
+  Button,
+  Typography,
+  TextField,
+  Box,
+  ListItemText,
+} from '@mui/material';
+import { styled } from '@mui/system';
 
-Modal.setAppElement('#root');
+const StyledBox = styled(Box)({
+  height: 'auto',
+  border: '1px solid #eee',
+  padding: '5px',
+  borderRadius: '5px',
+  background: 'white',
+});
 
 function TopicNode({ data }) {
   const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -25,55 +39,35 @@ function TopicNode({ data }) {
   };
 
   const ListItem = ({ keyName }) => (
-    <li>
-      {data[keyName]}{' '}
-      <button onClick={() => openModal(keyName)} style={{ fontSize: '1rem' }}>
-        Edit
-      </button>
-    </li>
+    <ListItemText
+      primary={data[keyName]}
+      secondary={
+        <Button size="small" onClick={() => openModal(keyName)}>
+          Edit
+        </Button>
+      }
+    />
   );
 
   return (
-    <div
-      style={{
-        height: 'auto',
-        border: '1px solid #eee',
-        padding: '5px',
-        borderRadius: '5px',
-        background: 'white',
-      }}
-    >
-      <section
-        style={{
-          height: 'auto',
-          width: 'auto',
-          fontSize: '1rem',
-        }}
-      >
-        <h1>{data.topic}</h1>
-        <ul
-          style={{
-            listStyleType: 'none',
-          }}
-        >
-          <strong>
-            <li>{data.q1}</li>
-          </strong>
-          <ListItem keyName="a1" />
-          <br />
-          <strong>
-            <li>{data.q2}</li>
-          </strong>
-          <ListItem keyName="a2" />
-          <br />
-          <strong>
-            <li>{data.q3}</li>
-          </strong>
-          <ListItem keyName="a3" />
-          <br />
-        </ul>
-      </section>
-      <div>
+    <StyledBox>
+      <Typography variant="h1">{data.topic}</Typography>
+      <Box component="ul" sx={{ listStyleType: 'none' }}>
+        <Typography variant="h5">
+          <li>{data.q1}</li>
+        </Typography>
+        <ListItem keyName="a1" />
+        <Typography variant="h5">
+          <li>{data.q2}</li>
+        </Typography>
+        <ListItem keyName="a2" />
+        <Typography variant="h5">
+          <li>{data.q3}</li>
+        </Typography>
+        <ListItem keyName="a3" />
+      </Box>
+
+      <Box>
         <Handle
           id="top"
           type="target"
@@ -98,36 +92,42 @@ function TopicNode({ data }) {
           position={Position.Left}
           isConnectable={true}
         />
-      </div>
+      </Box>
 
       <Modal
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        style={{
-          content: {
-            top: '50%',
-            left: '50%',
-            right: 'auto',
-            bottom: 'auto',
-            marginRight: '-50%',
-            transform: 'translate(-50%, -50%)',
-          },
-        }}
+        open={modalIsOpen}
+        onClose={closeModal}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+        style={{ alignItems: 'center', justifyContent: 'center' }}
       >
-        <h2>Edit Item</h2>
-        <textarea
-          value={tempValue}
-          onChange={(e) => setTempValue(e.target.value)}
-          style={{ width: '100%', height: '100px' }}
-        />
-        <div style={{ textAlign: 'right', marginTop: '10px' }}>
-          <button onClick={closeModal} style={{ marginRight: '10px' }}>
-            Cancel
-          </button>
-          <button onClick={confirmChanges}>Confirm</button>
-        </div>
+        <Box sx={{ width: '80%', bgcolor: 'background.paper', p: 2 }}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Edit Item
+          </Typography>
+          <TextField
+            id="modal-modal-description"
+            multiline
+            rows={4}
+            value={tempValue}
+            onChange={(e) => setTempValue(e.target.value)}
+            fullWidth
+          />
+          <Box sx={{ mt: 2, display: 'flex', justifyContent: 'flex-end' }}>
+            <Button variant="contained" onClick={closeModal} sx={{ mr: 1 }}>
+              Cancel
+            </Button>
+            <Button
+              variant="contained"
+              onClick={confirmChanges}
+              color="primary"
+            >
+              Confirm
+            </Button>
+          </Box>
+        </Box>
       </Modal>
-    </div>
+    </StyledBox>
   );
 }
 
